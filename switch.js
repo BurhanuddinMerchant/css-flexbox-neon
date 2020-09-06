85102c7592ce794e673b0d7c8fea3ce3216853ed
BURHANUDDIN <Bmerchant945@gmail.com>
2020-09-06 17:17:41 +0530

    changed the js controlling the change in css

--- switch.js
+++ switch.js
@@ -4,9 +4,9 @@ switchOn = () => {
   const cssTag = document.getElementsByClassName("css-tag");
   console.log(cssTag[0].href);
   if (isDark) {
-    cssTag[0].href = "http://127.0.0.1:5500/flexbox2/style2.css";
+    cssTag[0].href = "style2.css";
   } else {
-    cssTag[0].href = "http://127.0.0.1:5500/flexbox2/style.css";
+    cssTag[0].href = "style.css";
   }
   isDark = !isDark;
 };
