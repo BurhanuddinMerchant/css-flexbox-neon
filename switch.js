001e99dffe4bf65296eb4ab4e654b66d99f43cb3
BURHANUDDIN <Bmerchant945@gmail.com>
2020-09-06 17:30:02 +0530

    changed the code for switching css

--- switch.js
+++ switch.js
@@ -1,12 +1,26 @@
 let isDark = true;
 
+// switchOn = () => {
+//   const cssTag = document.getElementsByClassName("css-tag");
+//   if (isDark) {
+//     cssTag.href = "style2.css";
+//   } else {
+//     cssTag.href = "style.css";
+//   }
+//   isDark = !isDark;
+// };
 switchOn = () => {
-  const cssTag = document.getElementsByClassName("css-tag");
-  console.log(cssTag[0].href);
+  var oldlink = document.getElementsByTagName("link").item(0);
+
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("type", "text/css");
+
   if (isDark) {
-    cssTag[0].href = "style2.css";
+    newlink.setAttribute("href", "style2.css");
   } else {
-    cssTag[0].href = "style.css";
+    newlink.setAttribute("href", "style.css");
   }
   isDark = !isDark;
+  document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 };
